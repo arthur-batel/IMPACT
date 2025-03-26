2a5650e3ecb1bdd9fc1bdf3b1e3bb6f0f9e36477
Arthur BATEL <arthur.batel@insa-lyon.fr>
2025-03-26 14:23:39 +0100

    cloning R_t for safety

--- IMPACT/model/IMPACT.py
+++ IMPACT/model/IMPACT.py
@@ -178,7 +178,7 @@ class IMPACTModel(nn.Module):
 
         # ------ None learnable parameters
         # Modality mask creation + mod_per_item
-        R_t = self.R
+        R_t = self.R.clone()
         R_t[R_t == 0] = self.R_valid[R_t == 0]
         R_t = R_t.T - 1
         self.register_buffer('nb_modalities',
