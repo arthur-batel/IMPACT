66806f66e9c204bc2a4ad8bf6ba2c939d75ea75d
Arthur BATEL <arthur.batel@insa-lyon.fr>
2025-04-01 15:02:00 +0200

    setup version .19

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lirisimpact',
-    version='2.0.18',
+    version='2.0.19',
     author='Arthur BATEL',
     author_email='arthur.batel@insa-lyon.fr',
     packages=find_packages(),
