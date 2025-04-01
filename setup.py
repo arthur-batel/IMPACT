aa0e72d00f98d0a1e62b56dfdbc8e90c646d6732
Arthur BATEL <arthur.batel@insa-lyon.fr>
2025-04-01 15:33:39 +0200

    metadata cleaning fro CAT compatibility

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lirisimpact',
-    version='2.0.19',
+    version='2.0.20',
     author='Arthur BATEL',
     author_email='arthur.batel@insa-lyon.fr',
     packages=find_packages(),
