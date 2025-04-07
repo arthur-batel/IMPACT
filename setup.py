e3efe4e7e5ebe4c0f0a5916d0a4bde01b92eccbb
Arthur BATEL <arthur.batel@insa-lyon.fr>
2025-04-07 10:30:39 +0200

    version 2.0.24 with various dependencies handling

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
+import os
 from setuptools import setup, find_packages
+
+SKIP_DEPENDENCIES = os.getenv("SKIP_INSTALL_REQUIRES", "0") == "1"
+
+install_requires = [] if SKIP_DEPENDENCIES else [
+    'torch',
+    'vegas',
+    'numpy',
+    'pandas',
+    'scikit-learn',
+    'scipy',
+    'tqdm',
+    'numba',
+    'tensorboardX',
+],
 
 setup(
     name='lirisimpact',
-    version='2.0.23',
+    version='2.0.24',
     author='Arthur BATEL',
     author_email='arthur.batel@insa-lyon.fr',
     packages=find_packages(),
@@ -10,17 +25,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
     url='https://github.com/arthur-batel/IMPACT.git',
-    install_requires=[
-        'torch',
-        'vegas',
-        'numpy',
-        'pandas',
-        'scikit-learn',
-        'scipy',
-        'tqdm',
-        'numba',
-        'tensorboardX',
-    ],  # And any other dependencies foo needs
+    install_requires=install_requires,# And any other dependencies foo needs
     entry_points={
     },
     classifiers=[
