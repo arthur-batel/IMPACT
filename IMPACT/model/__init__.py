4215de559f2ebbea55032321ace2346d63008203
Arthur Batel <arthur.batel@insa-lyon.fr>
2025-05-21 15:55:37 +0200

    Extending init.py for greater outisde modification

--- IMPACT/model/__init__.py
+++ IMPACT/model/__init__.py
@@ -1,3 +1,3 @@
 from .abstract_model import AbstractModel
-from .IMPACT import IMPACT, resp_to_mod, mod_to_resp
+from .IMPACT import IMPACT, resp_to_mod, mod_to_resp, IMPACTModel, IMPACTModel_low_mem, custom_loss, custom_loss_low_mem
 from .MIRT import MIRT
